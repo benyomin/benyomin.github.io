cc7fb18e288e7e6cb9f18cb2bbb7db4be9cdfaa4
בנימן הגלילי <benyominhagalili@mail.ru>
2016-09-12 14:37:36 +0300

    removed biblatex chicago b/c of error

--- functionalExamples.R
+++ functionalExamples.R
@@ -1,11 +1,19 @@
 #functional R examples
 # http://adv-r.had.co.nz/Functional-programming.html
 
+# instructions to make a package
+# https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
 library("devtools")
-
+devtools::install_github("klutometis/roxygen")
+library(roxygen2)
+install("thesisProposal")
+?cat_function
+document()
 z <- c(3,4,3512,3,-999)
 y <- c(2,4,3426,44,-99)
-z <- rbind(z,y)
+x <- rbind(z,y)
+x
+
 fix_missing <- function(x) {
   x[x==-99] <- NA
   x}
@@ -46,3 +54,76 @@ fix_missing_99(-99,-999,300,30,-99)
 summary <- function(x) {
   c(mean(x),median(x),sd(x),mad(x),IQR(x))}
 lapply(df,summary)
+
+summary <- function(x) {
+  c(mean(x, na.rm= TRUE),
+  median(x,na.rm=TRUE),
+  sd(x,na.rm= TRUE))
+}
+
+  ## factor out repeated code
+## store functions in lists
+
+summary <- function(x) {
+  funs <- c(mean,median,sd,mad,IQR)
+  lapply(funs,function(f) f(x,na.rm=TRUE))
+}
+
+
+## functions have:
+
+
+#formals
+formals(function(x=4) g(x) + h(x))
+#body
+body(function(x=4 g(x) + h(x))
+
+#environment
+  environment(function(x=4 g(x) + h(x))
+
+    ## anonymous functions get extra parens
+  (function(x) x+3)(10)
+  ## like
+  f <- function(x) x+3
+  f(10)
+
+
+  set <- 0:10
+
+  set
+
+  sqPaynito <- function(x) {
+    x^2
+    }
+  sqPaynito(set)
+
+  egHad <- function(x){
+sqPaynito(x) -x
+
+  }
+egHad(set)
+
+integrate((function(set) sqPaynito(set) -set), lower=0, upper = 300)
+
+
+##closures
+
+power <- function(exponent) {
+  function(x) {
+    x ^ exponent}}
+
+square <- power(2)
+
+square(4)
+
+cube(3)
+cube <- power(3)
+cube(100)
+
+as.list(environment(square))
+
+as.list(environment(cube))
+install.packages("pryr")
+library(pryr)
+unenclose(square)
+unenclose(cube)
