f4e54e71579a33c01d4747c6981b55cc15278442
בנימן הגלילי <benyominhagalili@mail.ru>
2016-09-12 16:26:47 +0300

    mygit shortcut push

--- gh-publisher-scripts/build.sh
+++ gh-publisher-scripts/build.sh
@@ -3,11 +3,12 @@
 #
 # You have the following variables available:
 #
-GH_PUBLISHER_PROJECT_DIR - ".."
-GH_PUBLISHER_SCRIPTS_DIR - "."
+#GH_PUBLISHER_PROJECT_DIR - ".."
+#GH_PUBLISHER_SCRIPTS_DIR - "."
 #
 # The current working directory is $GH_PUBLISHER_PROJECT_DIR.
 #
 
-cd "$GH_PUBLISHER_PROJECT_DIR"
+#cd "$GH_PUBLISHER_PROJECT_DIR"
+echo running build.sh
 make
