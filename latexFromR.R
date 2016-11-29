fdc3c06d16c9cb87e50e03b2c36a13952bab859c
בנימן הגלילי <benyominhagalili@mail.ru>
2016-11-29 18:21:36 +0200

    add all

--- latexFromR.R
+++ latexFromR.R
@@ -1,9 +1,9 @@
 ## tools::texi2pdf('./marco.tex', clean = FALSE, quiet = TRUE,
 ##                           texi2dvi = getOption("texi2dvi"),
 ##                           texinputs = NULL, index = TRUE)
-
+## version 0.3b1
 ##   Not making .tex with R
 ## minimal example
 install.packages("knitr")
 library(knitr)
-knit('001-minimal.Rmd')
+knit('workingPaperNov2016.Rmd')
