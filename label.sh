90dbee6068e45ef6e45d55297643f87786f4b157
Travis-CI <jon@msnbs.co.uk>
2016-12-01 23:49:58 +0200

    label

--- label.sh
+++ label.sh
@@ -3,12 +3,13 @@
 #
 today=`date +%Y-%m-%d.%H:%M:%S`
 filename="paperVersion$today.pdf"
-echo $filename;
-cd versions
-cp ../publishedVersion.pdf ./$filename
-ls
+#echo $filename;
+cp publishedVersion.pdf $filename.pdf
+git checkout --deploy "versions/readme.org"
+mv $filename.pdf versions/$filename.pdf
 git config user.name "Travis-CI"
 git config user.email "jon@msnbs.co.uk"
+cd versions
 git add $filename
 git commit -m "$filename"
 git push --force --quiet "https://${GH_TOKEN}@github.com/benyomin/benyomin.github.io.git" master:deploy > /dev/null 2>&1
