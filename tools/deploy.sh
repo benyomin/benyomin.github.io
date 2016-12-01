e6da4a91b161c68780d5eb213228bc318f662116
בנימן הגלילי <benyominhagalili@mail.ru>
2016-12-01 21:34:26 +0200

    add deploy after success hook

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,4 +1,8 @@
 #/bin/bash
-
- echo This needs to copy the pdf to the review dir.
-
+git checkout -b gh-pages
+git config user.name "Travis Auto"
+git config user.email "automated@logging.err"
+mv publishedVersion.pdf ./proofs/publishedVersion.pdf
+git add .
+git commit -m "new successful version"
+git push
