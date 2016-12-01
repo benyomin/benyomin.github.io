cbe409559ea29f5743c4155331e669152058be4b
benyomin <jon@msnbs.co.uk>
2016-12-02 00:23:43 +0200

    travis

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -7,7 +7,6 @@ filename="paperVersion$today.pdf"
 cp publishedVersion.pdf $filename
 #git checkout branch -- file
 #git checkout branch -- "path/file.txt"
-git checkout deploy -- "versions/readme.org"
 mv $filename versions/$filename
 git config user.name "benyomin"
 git config user.email "jon@msnbs.co.uk"
@@ -15,6 +14,4 @@ cd versions
 git add $filename
 git commit -m "$filename"
 git push --force --quiet "https://${GH_TOKEN}@github.com/benyomin/benyomin.github.io.git" master:deploy > /dev/null 2>&1
-cd ..
-rm -rd versions
-
+rm *.*
