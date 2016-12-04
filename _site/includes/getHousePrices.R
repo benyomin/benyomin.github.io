f4ec3cb82b77709f767cd7dc2452eb49f2f63134
בנימן הגלילי <benyominhagalili@mail.ru>
2016-12-04 22:05:16 +0200

    index from backup

---
+++ _site/includes/getHousePrices.R
@@ -0,0 +1,10 @@
+## checks if files already exists, 
+## if not imports them using helper function from
+## install_github("benyomin/censusFunctions")
+
+downloadHousingPrices(16)
+downloadHousingPrices(14)
+downloadHousingPrices(13)
+downloadHousingPrices(11)
+downloadHousingPrices(09)
+downloadHousingPrices(07)
