15bed8995db0804457cd1528a7eef5a9f19ffc79
benyomin <jon@msnbs.co.uk>
2016-12-04 14:45:17 +0200

    scripts

---
+++ gh-publisher-scripts/build.sh
@@ -0,0 +1,15 @@
+#
+# Use this file to configure your build.
+#
+# You have the following variables available:
+#
+#GH_PUBLISHER_PROJECT_DIR - ".."
+#GH_PUBLISHER_SCRIPTS_DIR - "."
+#
+# The current working directory is $GH_PUBLISHER_PROJECT_DIR.
+#
+#cd "$GH_PUBLISHER_PROJECT_DIR"
+echo running build.sh
+echo this would normally compile the .tex to .pdf, but that was done with rmarkdown::render
+#./gh-publisher.sh
+
