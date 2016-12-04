47b3cbadade2a8defec81589fed7f6710a7bda03
בנימן הגלילי <benyominhagalili@mail.ru>
2016-12-04 19:58:58 +0200

    missing .sty

--- includes/getHousePrices.R
+++ includes/getHousePrices.R
@@ -1,3 +1,6 @@
+## checks if files already exists, 
+## if not imports them using helper function from
+## install_github("benyomin/censusFunctions")
 downloadHousingPrices(16)
 downloadHousingPrices(14)
 downloadHousingPrices(13)
